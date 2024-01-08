8c3efdb32f53c7eba92f89a177726e9729562090
Blushyes <Blushyes@gmail.com>
2024-01-09 00:26:20 +0800

    重构登录逻辑

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from concurrent.futures import ThreadPoolExecutor
 
 from context.main import context
-from core.login import login
 from core.plugin import SimplePluginManager
 
 # 1472871866
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with ThreadPoolExecutor(max_workers=8) as executor:
         # 登录
-        login(context.uid)
+        context.login_manager.login(context.uid)
 
         # 创建插件管理器
         plugin_manager = SimplePluginManager(executor)
--- plugins/reply_myself/main.py
+++ plugins/reply_myself/main.py
@@ -3,12 +3,12 @@ from plugins.reply_myself.reply_myself import ReplyMyself
 
 
 def start():
-    if not context.config_manager.exists('global', 'reply_myself_switch'):
-        log.error('缺少config.ini中的reply_myself_switch配置项')
+    if not context.config_manager.exists("global", "reply_myself_switch"):
+        log.error("缺少config.ini中的reply_myself_switch配置项")
         return
 
-    if context.config_manager.get_config('global', 'reply_myself_switch') != 'ON':
-        log.info('回复评论区功能未开启')
+    if context.config_manager.get_config("global", "reply_myself_switch") != "ON":
+        log.info("回复评论区功能未开启")
         return
     reply_self = ReplyMyself(context.uid)
     reply_self.start_loop()
